3afcf545d452b3599695878235e81f56fdf586e6
Ryan Delaney <rydelaney@hubspot.com>
2023-10-12 12:01:25 +0100

    Try and go back in history before closing

--- content.js
+++ content.js
@@ -1,3 +1,8 @@
 setTimeout(function() {
-  chrome.runtime.sendMessage({}, function() {});
-}, 5000);
+  window.history.go(-1);
+  setTimeout(() => {
+    // If this code is still running, we haven't navigated away from this page so history is likely empty
+    // Close the tab
+    chrome.runtime.sendMessage({}, function() {});
+  }, 700);
+}, 1000);
